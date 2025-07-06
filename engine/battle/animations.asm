d9049a8282cadced30e59360c926d68e1b55a9d0
CreamElDudJafar <jahern2398@gmail.com>
2025-07-06 10:20:43 -0500

    Removed Color Attacks... Again....
    Keep finding bugs

--- engine/battle/animations.asm
+++ engine/battle/animations.asm
@@ -454,7 +454,7 @@ MoveAnimationContent:
 	xor a
 	vc_hook Stop_reducing_move_anim_flashing_Haze_Hyper_Beam
 	ld [wSubAnimSubEntryAddr], a
-;	ld [wUnusedMoveAnimByte], a
+	ld [wUnusedMoveAnimByte], a
 	ld [wSubAnimTransform], a
 	dec a ; NO_MOVE - 1
 	ld [wAnimSoundID], a
@@ -565,14 +565,13 @@ AnimationShakeScreenHorizontallySlow:
 	ret
 
 SetAnimationPalette:
-	ld b, $e4
 	ld a, [wOnSGB]
 	and a
-;	ld a, $e4
+	ld a, $e4
 	jr z, .notSGB
-;	ld a, $f0
-;	ld [wAnimPalette], a
-;	ld b, $e4
+	ld a, $f0
+	ld [wAnimPalette], a
+	ld b, $e4
 	ld a, [wAnimationID]
 	cp TRADE_BALL_DROP_ANIM
 	jr c, .next
@@ -588,7 +587,6 @@ SetAnimationPalette:
 	ldh [rOBP1], a
 	call UpdateGBCPal_OBP0
 	call UpdateGBCPal_OBP1
-	predef SetAttackAnimPal
 	ret
 .notSGB
 	ld a, $e4
@@ -726,18 +724,14 @@ DoSpecialEffectByAnimationId:
 INCLUDE "data/battle_anims/special_effects.asm"
 
 DoBallTossSpecialEffects:
-	ld a, [wCurPartySpecies]
-	cp 3
+	ld a, [wCurItem]
+	cp ULTRA_BALL + 1 ; is it a Master Ball or Ultra Ball?
 	jr nc, .skipFlashingEffect
-	ld a, [wSubAnimCounter]
-	cp 1
-	jr z, .skipFlashingEffect
 .flashingEffect ; do a flashing effect if it's Master Ball or Ultra Ball
 	ldh a, [rOBP0]
 	xor %00111100 ; complement colors 1 and 2
 	ldh [rOBP0], a
-;	call UpdateGBCPal_OBP0
-	predef SetAttackAnimPal
+	call UpdateGBCPal_OBP0
 .skipFlashingEffect
 	ld a, [wSubAnimCounter]
 	cp 11 ; is it the beginning of the subanimation?
@@ -754,14 +748,12 @@ DoBallTossSpecialEffects:
 	ret nz
 ; if the enemy pokemon is the Ghost Marowak, make it dodge during the last 3 frames
 	ld a, [wSubAnimCounter]
-;	cp 3
-;	jr z, .moveGhostMarowakLeft
-;	cp 2
-;	jr z, .moveGhostMarowakLeft
-;	cp 1
-;	ret nz
-	cp 4
-	ret nc
+	cp 3
+	jr z, .moveGhostMarowakLeft
+	cp 2
+	jr z, .moveGhostMarowakLeft
+	cp 1
+	ret nz
 .moveGhostMarowakLeft
 	hlcoord 17, 0
 	ld de, 20
@@ -780,7 +772,7 @@ DoBallTossSpecialEffects:
 	ret
 .isTrainerBattle ; if it's a trainer battle, shorten the animation by one frame
 	ld a, [wSubAnimCounter]
-	cp 2
+	cp 3
 	ret nz
 	dec a
 	ld [wSubAnimCounter], a
@@ -1129,13 +1121,13 @@ AnimationDarkenMonPalette:
 	lb bc, $f9, $f4
 	jr SetAnimationBGPalette
 
-;AnimationUnusedPalette1:
-;	lb bc, $fe, $f8
-;	jr SetAnimationBGPalette
+AnimationUnusedPalette1:
+	lb bc, $fe, $f8
+	jr SetAnimationBGPalette
 
-;AnimationUnusedPalette2:
-;	lb bc, $ff, $ff
-;	jr SetAnimationBGPalette
+AnimationUnusedPalette2:
+	lb bc, $ff, $ff
+	jr SetAnimationBGPalette
 
 AnimationResetScreenPalette:
 ; Restores the screen's palette to the normal palette.
@@ -1193,12 +1185,12 @@ AnimationWaterDropletsEverywhere:
 	ld a, 16
 	ld [wBaseCoordY], a
 	ld a, 0
-;	ld [wUnusedWaterDropletsByte], a
+	ld [wUnusedWaterDropletsByte], a
 	call _AnimationWaterDroplets
 	ld a, 24
 	ld [wBaseCoordY], a
 	ld a, 32
-;	ld [wUnusedWaterDropletsByte], a
+	ld [wUnusedWaterDropletsByte], a
 	call _AnimationWaterDroplets
 	dec d
 	jr nz, .loop
