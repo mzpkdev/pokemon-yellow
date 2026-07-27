6732e8fda25e3a1608713402c10504d26731e3bc
mzpkdev <mateusz@mzpk.dev>
2026-07-27 14:32:05 +0100

    Merge late wild encounter progression

--- data/wild/maps/PokemonTower6F.asm
+++ data/wild/maps/PokemonTower6F.asm
@@ -1,12 +1,12 @@
 PokemonTower6FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 22, GASTLY
 	db 23, GASTLY
 	db 24, GASTLY
 	db 25, GASTLY
 	db 26, GASTLY
-	db 25, CUBONE
-	db 26, CUBONE
+	db 27, GASTLY
+	db 23, GASTLY
+	db 24, CUBONE
 	db 28, HAUNTER
 	db 29, HAUNTER
 	db 30, MAROWAK
--- data/wild/maps/VictoryRoad2F.asm
+++ data/wild/maps/VictoryRoad2F.asm
@@ -1,15 +1,15 @@
 VictoryRoad2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 42, GRAVELER
-	db 43, RHYHORN
-	db 43, GRAVELER
+	db 46, GRAVELER
+	db 42, RHYHORN
+	db 45, GRAVELER
 	db 44, GOLBAT
-	db 40, GOLEM
-	db 44, GOLEM
+	db 45, GOLEM
+	db 46, GOLEM
 	db 45, ONIX
 	db 47, MACHAMP
-	db 46, HITMONCHAN
-	db 46, HITMONLEE
+	db 48, HITMONCHAN
+	db 48, HITMONLEE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
