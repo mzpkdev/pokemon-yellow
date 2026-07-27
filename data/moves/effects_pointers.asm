c7eaca3c9b018943b53e24fae3f3d0c34f681843
mzpkdev <mateusz@mzpk.dev>
2026-07-27 21:17:16 +0100

    Support guaranteed Speed drops for damaging moves

--- data/moves/effects_pointers.asm
+++ data/moves/effects_pointers.asm
@@ -72,7 +72,7 @@ MoveEffectPointerTable:
 	dw StatModifierDownEffect    ; DEFENSE_DOWN_SIDE_EFFECT
 	dw StatModifierDownEffect    ; SPEED_DOWN_SIDE_EFFECT
 	dw StatModifierDownEffect    ; SPECIAL_DOWN_SIDE_EFFECT
-	dw StatModifierDownEffect    ; unused effect
+	dw StatModifierDownEffect    ; SPEED_DOWN_SIDE_EFFECT_CERT
 	dw StatModifierDownEffect    ; unused effect
 	dw StatModifierDownEffect    ; unused effect
 	dw StatModifierDownEffect    ; unused effect
