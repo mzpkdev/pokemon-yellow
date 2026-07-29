4d974a8e6659cf28e7efa2e8b8acb91dbab5832f
mzpkdev <mateusz@mzpk.dev>
2026-07-29 17:17:36 +0100

    feat: default move replacement to last slot

--- engine/pokemon/learn_move.asm
+++ engine/pokemon/learn_move.asm
@@ -152,14 +152,14 @@ TryingToLearn:
 	ld [hli], a ; wTopMenuItemY
 	ld a, 5
 	ld [hli], a ; wTopMenuItemX
-	xor a
+	ld a, [wNumMovesMinusOne]
 	ld [hli], a ; wCurrentMenuItem
 	inc hl
-	ld a, [wNumMovesMinusOne]
 	ld [hli], a ; wMaxMenuItem
 	ld a, A_BUTTON | B_BUTTON
 	ld [hli], a ; wMenuWatchedKeys
-	ld [hl], 0 ; wLastMenuItem
+	ld a, [wNumMovesMinusOne]
+	ld [hl], a ; wLastMenuItem
 	ld hl, hUILayoutFlags
 	set BIT_DOUBLE_SPACED_MENU, [hl]
 	call HandleMenuInput
